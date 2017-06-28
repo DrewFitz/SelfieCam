bb0ba2692900e7176ee97d13e4ee0e2bf765bddb
Drew Fitzpatrick <drwftz@gmail.com>
2017-06-28 16:47:33 -0400

    Boom shakka-lakka

--- SelfieCam/ViewController.swift
+++ SelfieCam/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var cameraController : UIImagePickerController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        prepareAndPresentCameraController()
+    }
+    
+    func prepareAndPresentCameraController() {
+        let camera = UIImagePickerController()
+        
+        camera.sourceType = .camera
+        camera.cameraDevice = .front
+        camera.delegate = self
+        camera.cameraViewTransform = CGAffineTransform(scaleX: -1.0, y: 1)
+        camera.showsCameraControls = true
+        camera.modalPresentationStyle = .fullScreen
+        
+        cameraController = camera
+        present(cameraController!, animated: false, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        for item in info {
+            if let image = item.value as? UIImage {
+                UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+                break
+            }
+        }
+        
+        picker.dismiss(animated: true, completion: {
+            DispatchQueue.main.async {
+                self.prepareAndPresentCameraController()
+            }
+        } )
+    }
 
 }
 
